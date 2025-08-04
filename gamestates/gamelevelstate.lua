1ece7af269a8d36c6f122fdfca036e3cc1bc1e24
Drew Harry <drew.harry@gmail.com>
2025-08-03 18:20:46 -0700

    Fixed pushing.

--- gamestates/gamelevelstate.lua
+++ gamestates/gamelevelstate.lua
@@ -11,87 +11,87 @@ local MyGameLevelState = spectrum.LevelState:extend "MyGameLevelState"
 
 --- @param display Display
 function MyGameLevelState:__new(display)
-   -- Construct a simple test map using MapBuilder.
-   -- In a complete game, you'd likely extract this logic to a separate module
-   -- and pass in an existing player object between levels.
-   local mapbuilder = prism.MapBuilder(prism.cells.Wall)
+    -- Construct a simple test map using MapBuilder.
+    -- In a complete game, you'd likely extract this logic to a separate module
+    -- and pass in an existing player object between levels.
+    local mapbuilder = prism.MapBuilder(prism.cells.Wall)
 
-   mapbuilder:drawRectangle(0, 0, 32, 32, prism.cells.Wall)
-   -- Fill the interior with floor tiles
-   mapbuilder:drawRectangle(1, 1, 31, 31, prism.cells.Floor)
-   -- Add a small block of walls within the map
-   mapbuilder:drawRectangle(5, 5, 7, 7, prism.cells.Wall)
-   -- Add a pit area to the southeast
-   mapbuilder:drawRectangle(20, 20, 25, 25, prism.cells.Pit)
+    mapbuilder:drawRectangle(0, 0, 32, 32, prism.cells.Wall)
+    -- Fill the interior with floor tiles
+    mapbuilder:drawRectangle(1, 1, 31, 31, prism.cells.Floor)
+    -- Add a small block of walls within the map
+    mapbuilder:drawRectangle(5, 5, 7, 7, prism.cells.Wall)
+    -- Add a pit area to the southeast
+    mapbuilder:drawRectangle(20, 20, 25, 25, prism.cells.Pit)
 
-   -- Place the player character at a starting location
-   mapbuilder:addActor(prism.actors.Player(), 12, 12)
+    -- Place the player character at a starting location
+    mapbuilder:addActor(prism.actors.Player(), 12, 12)
 
-   -- Build the map and instantiate the level with systems
-   local map, actors = mapbuilder:build()
-   local level = prism.Level(map, actors, {
-      prism.systems.Senses(),
-      prism.systems.Sight(),
-   })
+    -- Build the map and instantiate the level with systems
+    local map, actors = mapbuilder:build()
+    local level = prism.Level(map, actors, {
+        prism.systems.Senses(),
+        prism.systems.Sight(),
+    })
 
-   -- Initialize with the created level and display, the heavy lifting is done by
-   -- the parent class.
-   spectrum.LevelState.__new(self, level, display)
+    -- Initialize with the created level and display, the heavy lifting is done by
+    -- the parent class.
+    spectrum.LevelState.__new(self, level, display)
 end
 
 function MyGameLevelState:handleMessage(message)
-   spectrum.LevelState.handleMessage(self, message)
+    spectrum.LevelState.handleMessage(self, message)
 
-   -- Handle any messages sent to the level state from the level. LevelState
-   -- handles a few built-in messages for you, like the decision you fill out
-   -- here.
+    -- Handle any messages sent to the level state from the level. LevelState
+    -- handles a few built-in messages for you, like the decision you fill out
+    -- here.
 
-   -- This is where you'd process custom messages like advancing to the next
-   -- level or triggering a game over.
+    -- This is where you'd process custom messages like advancing to the next
+    -- level or triggering a game over.
 end
 
 --- @param primary Senses[] { curActor:getComponent(prism.components.Senses)}
 ---@param secondary Senses[]
 function MyGameLevelState:draw(primary, secondary)
-   if not self.decision then return end
-   
-   self.display:clear()
+    if not self.decision then return end
 
-   local position = self.decision.actor:getPosition()
-   if not position then return end
+    self.display:clear()
 
-   local x, y = self.display:getCenterOffset(position:decompose())
-   self.display:setCamera(x, y)
+    local position = self.decision.actor:getPosition()
+    if not position then return end
 
-   local primary, secondary = self:getSenses()
-   -- Render the level using the actor’s senses
-   self.display:putSenses(primary, secondary)
+    local x, y = self.display:getCenterOffset(position:decompose())
+    self.display:setCamera(x, y)
 
-   -- custom terminal drawing goes here!
+    local primary, secondary = self:getSenses()
+    -- Render the level using the actor’s senses
+    self.display:putSenses(primary, secondary)
 
-   -- Say hello!
-   self.display:putString(1, 1, "Hello prism!")
+    -- custom terminal drawing goes here!
 
-   -- Actually render the terminal out and present it to the screen.
-   -- You could use love2d to translate and say center a smaller terminal or
-   -- offset it for custom non-terminal UI elements. If you do scale the UI
-   -- just remember that display:getCellUnderMouse expects the mouse in the
-   -- display's local pixel coordinates
-   self.display:draw()
+    -- Say hello!
+    self.display:putString(1, 1, "Hello prism!")
 
-   -- custom love2d drawing goes here!
+    -- Actually render the terminal out and present it to the screen.
+    -- You could use love2d to translate and say center a smaller terminal or
+    -- offset it for custom non-terminal UI elements. If you do scale the UI
+    -- just remember that display:getCellUnderMouse expects the mouse in the
+    -- display's local pixel coordinates
+    self.display:draw()
+
+    -- custom love2d drawing goes here!
 end
 
 -- Maps string actions from the keybinding schema to directional vectors.
 local keybindOffsets = {
-   ["move up"] = prism.Vector2.UP,
-   ["move left"] = prism.Vector2.LEFT,
-   ["move down"] = prism.Vector2.DOWN,
-   ["move right"] = prism.Vector2.RIGHT,
-   ["move up-left"] = prism.Vector2.UP_LEFT,
-   ["move up-right"] = prism.Vector2.UP_RIGHT,
-   ["move down-left"] = prism.Vector2.DOWN_LEFT,
-   ["move down-right"] = prism.Vector2.DOWN_RIGHT,
+    ["move up"] = prism.Vector2.UP,
+    ["move left"] = prism.Vector2.LEFT,
+    ["move down"] = prism.Vector2.DOWN,
+    ["move right"] = prism.Vector2.RIGHT,
+    ["move up-left"] = prism.Vector2.UP_LEFT,
+    ["move up-right"] = prism.Vector2.UP_RIGHT,
+    ["move down-left"] = prism.Vector2.DOWN_LEFT,
+    ["move down-right"] = prism.Vector2.DOWN_RIGHT,
 }
 
 -- The input handling functions act as the player controller’s logic.
@@ -99,30 +99,42 @@ local keybindOffsets = {
 -- action and set it in the decision object. It will then be executed by
 -- the level. This is a similar pattern to the example KoboldController.
 function MyGameLevelState:keypressed(key, scancode)
-   -- handles opening geometer for us
-   spectrum.LevelState.keypressed(self, key, scancode)
+    -- handles opening geometer for us
+    spectrum.LevelState.keypressed(self, key, scancode)
 
-   local decision = self.decision
-   if not decision then return end
+    local decision = self.decision
+    if not decision then return end
 
-   local owner = decision.actor
+    local owner = decision.actor
 
-   -- Resolve the action string from the keybinding schema
-   local action = keybindings:keypressed(key)
+    -- Resolve the action string from the keybinding schema
+    local action = keybindings:keypressed(key)
 
-   -- Attempt to translate the action into a directional move
-   if keybindOffsets[action] then
-      local destination = owner:getPosition() + keybindOffsets[action]
+    -- Attempt to translate the action into a directional move
+    if keybindOffsets[action] then
+        local destination = owner:getPosition() + keybindOffsets[action]
 
-      local move = prism.actions.Move(owner, destination)
-      if self.level:canPerform(move) then
-         decision:setAction(move)
-         return
-      end
-   end
+        local move = prism.actions.Move(owner, destination)
+        if self.level:canPerform(move) then
+            decision:setAction(move)
+            return
+        end
 
-   -- Handle waiting
-   if action == "wait" then decision:setAction(prism.actions.Wait(self.decision.actor)) end
+        -- will drop out above if a valid move action
+        -- ohhh, above will fail if the move is
+        local target = self.level:query()
+            :at(destination:decompose())
+            :first()
+
+        print("owner: ", owner)
+        local shoot = prism.actions.Shoot(owner, target)
+        if self.level:canPerform(shoot) then
+            decision:setAction(shoot)
+        end
+    end
+
+    -- Handle waiting
+    if action == "wait" then decision:setAction(prism.actions.Wait(self.decision.actor)) end
 end
 
 return MyGameLevelState
---
+++ modules/game/actions/shoot.lua
@@ -0,0 +1,37 @@
+local ShootTarget = prism.Target()
+    :with(prism.components.Collider)
+    :range(1)
+    :sensed()
+
+local Shoot = prism.Action:extend("ShootAction")
+Shoot.name = "Shoot"
+Shoot.targets = { ShootTarget }
+Shoot.requireComponents = {
+    prism.components.Controller
+}
+
+function Shoot:canPerform(level)
+    return true
+end
+
+function Shoot:perform(level, shot)
+    print("SHOOTING")
+    local direction = (shot:getPosition() - self.owner:getPosition())
+
+    local mask = prism.Collision.createBitmaskFromMovetypes { "walk" }
+
+    -- pushes back 3 steps
+    -- our actual shot will not do this (or we will parameterize it?)
+    -- but it's fine for now
+    for _ = 1, 3 do
+        local nextpos = shot:getPosition() + direction
+        print("nextpos: ", nextpos.x, ",", nextpos.y)
+
+        if not level:getCellPassable(nextpos.x, nextpos.y, mask) then break end
+        if not level:hasActor(shot) then break end
+
+        level:moveActor(shot, nextpos)
+    end
+end
+
+return Shoot
