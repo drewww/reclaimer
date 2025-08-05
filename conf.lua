994b7b5c486f9f10107545a4d888c869c53934f1
Drew Harry <drew.harry@gmail.com>
2025-08-04 19:39:38 -0700

    Messed with DPI settings, but to no avail. Leaving the values behind to perhaps mess with later.

--- conf.lua
+++ conf.lua
@@ -1,8 +1,11 @@
 --- @diagnostic disable-next-line
 function love.conf(t)
-   t.window.vsync = 0 -- Enable vsync (1 by default)
-   t.window.width = 960
-   t.window.height = 540
-   t.window.usedpiscale = false
-   -- Other configurations...
+    t.window.vsync = 0 -- Enable vsync (1 by default)
+    t.window.width = 960
+    t.window.height = 540
+    t.window.title = "exterminator"
+    t.highdpi = false            -- Enable high-dpi mode for the window on a Retina display (boolean)
+    t.window.usedpiscale = false -- Enable automatic DPI scaling when highdpi is set to true as well (boolean)
+    t.version = "12.0"           -- The LÖVE version this game was made for (string)
+    -- Other configurations...
 end
