ed968074538ca24257fb4c512e37aa37167c4929
Drew Harry <drew.harry@gmail.com>
2025-09-02 21:24:25 -0700

    Reset to a totally sparse level with no fancier generation logic.

--- levelgen/levelgen.lua
+++ levelgen/levelgen.lua
@@ -1,5 +1,3 @@
-local PARTITIONS = 3
-
 --- @param rng RNG
 --- @param player Actor
 --- @param width integer
@@ -7,99 +5,16 @@ local PARTITIONS = 3
 return function(rng, player, width, height)
    local builder = prism.LevelBuilder(prism.cells.Wall)
 
-   local nox, noy = rng:random(1, 10000), rng:random(1, 10000)
-   for x = 1, width do
-      for y = 1, height do
-         -- local noise = love.math.perlinNoise(x / 5 + nox, y / 5 + noy)
-         local noise = love.math.noise(x / 5 + nox + y / 5 + noy)
-         local cell = noise > 0.5 and prism.cells.Wall or prism.cells.Pit
+   -- go back to basics. fill all of width/height with floor.
+   builder:rectangle("fill", 1, 1, width - 1, height - 1, prism.cells.Floor)
 
-         builder:set(x, y, cell())
-      end
-   end
 
-   --- @type table<number, Rectangle>
-   local rooms = {}
-
-   local missing = prism.Vector2(rng:random(0, PARTITIONS - 1), rng:random(0, PARTITIONS - 1))
-
-   local pw, ph = math.floor(width / PARTITIONS), math.floor(height / PARTITIONS)
-
-   local minrw, minrh = math.floor(pw / 3), math.floor(ph / 3)
-   local maxrw, maxrh = pw - 2, ph - 2
-
-   for px = 0, PARTITIONS - 1 do
-      for py = 0, PARTITIONS - 1 do
-         if not missing:equals(px, py) then
-            local rw = rng:random(minrw, maxrw)
-            local rh = rng:random(minrh, maxrh)
-            local x = rng:random(px * pw + 1, (px + 1) * pw - rw - 1)
-            local y = rng:random(py * ph + 1, (py + 1) * ph - rh - 1)
-
-            local roomRect = prism.Rectangle(x, y, rw, rh)
-            rooms[prism.Vector2._hash(px, py)] = roomRect
-
-            builder:rectangle("fill", x, y, x + rw, y + rh, prism.cells.Floor)
-         end
-      end
-   end
-
-   -- Helper function to connect two points with an L-shaped hallway.
-   --- @param a Rectangle
-   --- @param b Rectangle
-   local function createLShapedHallway(a, b)
-      if not a or not b then return end
-
-      local ax, ay = a:center():floor():decompose()
-      local bx, by = b:center():floor():decompose()
-      -- Randomly choose one of two L-shaped tunnel patterns for variety.
-      if rng:random() > 0.5 then
-         builder:line(ax, ay, bx, ay, prism.cells.Floor)
-         builder:line(bx, ay, bx, by, prism.cells.Floor)
-      else
-         builder:line(ax, ay, ax, by, prism.cells.Floor)
-         builder:line(ax, by, bx, by, prism.cells.Floor)
-      end
-   end
-
-   for hash, currentRoom in pairs(rooms) do
-      local px, py = prism.Vector2._unhash(hash)
-
-      createLShapedHallway(currentRoom, rooms[prism.Vector2._hash(px + 1, py)])
-      createLShapedHallway(currentRoom, rooms[prism.Vector2._hash(px, py + 1)])
-   end
-
-   local startRoom
-   while not startRoom do
-      local x, y = rng:random(0, PARTITIONS - 1), rng:random(0, PARTITIONS - 1)
-      startRoom = rooms[prism.Vector2._hash(x, y)]
-   end
-
-   local playerPos = startRoom:center():floor()
+   local playerPos = prism.Vector2(3, 3)
    builder:addActor(player, playerPos.x, playerPos.y)
-
-   for _, room in pairs(rooms) do
-      if room ~= startRoom then
-         local cx, cy = room:center():floor():decompose()
-
-         builder:addActor(prism.actors.Bot(), cx, cy)
-         builder:addActor(prism.actors.Barrel(), cx + 1, cy + 1)
-      end
-   end
 
    builder:pad(1, prism.cells.Wall)
 
-   --- @type Rectangle[]
-   local availableRooms = {}
-   for _, room in pairs(rooms) do
-      if room ~= startRoom then table.insert(availableRooms, room) end
-   end
-
-   local stairRoom = availableRooms[rng:random(1, #availableRooms)]
-   local corners = stairRoom:toCorners()
-   local randCorner = corners[rng:random(1, #corners)]
-
-   builder:addActor(prism.actors.Stairs(), randCorner.x, randCorner.y)
+   -- builder:addActor(prism.actors.Stairs(), randCorner.x, randCorner.y)
 
    return builder
 end
