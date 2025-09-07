eceee3ec419ca1e29c7ddb7ffedf4ee82e909a7e
Drew Harry <drew.harry@gmail.com>
2025-09-07 09:34:30 -0700

    Added a scrap icon, drop scrap instead of weapons.
    TODO think about ammo dropping.

--- modules/game/actions/die.lua
+++ modules/game/actions/die.lua
@@ -6,16 +6,20 @@ local Game = require "game"
 function Die:perform(level)
    -- check if the dying actor has an inventory. if it does, drop the first item
    -- from its inventory at this position.
-   local inventory = self.owner:get(prism.components.Inventory)
-   if inventory and inventory.totalCount > 0 then
-      -- TODO how to make this random? might be a nice bit of query suger
-      local item = inventory:query(prism.components.Item):first()
-      level:addActor(item, self.owner:getPosition():decompose())
-
-      if item then
-         prism.logger.info("Adding inventory item on death: " .. item:getName())
-      end
+   --
+   if self.owner:has(prism.components.BotController) then
+      level:addActor(prism.actors.Scrap(), self.owner:getPosition():decompose())
    end
+   -- local inventory = self.owner:get(prism.components.Inventory)
+   -- if inventory and inventory.totalCount > 0 then
+   --    -- TODO how to make this random? might be a nice bit of query suger
+   --    local item = inventory:query(prism.components.Item):first()
+   --    level:addActor(item, self.owner:getPosition():decompose())
+
+   --    if item then
+   --       prism.logger.info("Adding inventory item on death: " .. item:getName())
+   --    end
+   -- end
 
    if not self.owner:has(prism.components.PlayerController) then
       Game.stats:increment("kills")
