7559b46c473b75917066a3a477269e9a73da6246
Drew Harry <drew.harry@gmail.com>
2025-09-07 11:12:30 -0700

    With updated prism, this works.

--- modules/game/actors/ammostack.lua
+++ modules/game/actors/ammostack.lua
@@ -1,16 +1,3 @@
--- TODO add an ammo type parameter to this, pass it into the ammo component.
--- prism.registerActor("AmmoStack", function(count)
---    return prism.Actor.fromComponents {
---       prism.components.Drawable { index = "a", color = prism.Color4.YELLOW },
---       prism.components.Health(1),
---       prism.components.Item({
---          stackable = prism.actors.AmmoStack, -- TODO work out how to make these stack separately by type.
---          stackCount = count or 1,
---          stackLimit = 99
---       })
---    }
--- end)
-
 AMMO_TYPES = {}
 
 local function registerAmmo(type)
@@ -32,7 +19,6 @@ local function registerAmmo(type)
 end
 
 -- table stores ACTOR constructors that we can pass into Item.stackable
-
 AMMO_TYPES["Pistol"] = registerAmmo("Pistol")
 AMMO_TYPES["Rocket"] = registerAmmo("Rocket")
 AMMO_TYPES["Shotgun"] = registerAmmo("Shotgun")
