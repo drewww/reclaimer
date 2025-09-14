56c6141836d701ccfa6132e071b21c320b4b7b50
Drew Harry <drew.harry@gmail.com>
2025-09-14 09:40:52 -0700

    Make sure stairs appear above fire.

--- modules/game/actors/stairs.lua
+++ modules/game/actors/stairs.lua
@@ -2,7 +2,7 @@ prism.registerActor("Stairs", function()
    return prism.Actor.fromComponents {
       prism.components.Name("Stairs"),
       prism.components.Position(),
-      prism.components.Drawable { index = STAIRS, color = COLOR_PLAYER },
+      prism.components.Drawable { index = STAIRS, color = COLOR_PLAYER, math.huge - 20 },
       prism.components.Stair(),
       prism.components.Remembered(),
    }
