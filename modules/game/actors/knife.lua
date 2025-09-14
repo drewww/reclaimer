892723f7596b3b2706af19d870a81e410a027356
Drew Harry <drew.harry@gmail.com>
2025-09-13 17:44:44 -0700

    Rename Knife to push.

--- modules/game/actors/knife.lua
+++ modules/game/actors/knife.lua
@@ -1,6 +1,6 @@
 prism.registerActor("Knife", function()
    return prism.Actor.fromComponents {
-      prism.components.Name("Knife"),
+      prism.components.Name("Spin Push"),
       prism.components.Position(),
       prism.components.Drawable { index = GUN, color = prism.Color4.YELLOW },
       prism.components.Item(),
